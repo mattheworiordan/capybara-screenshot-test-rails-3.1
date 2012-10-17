565f423ad59e106f4931c0834b4e481d9519f421
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-10-17 20:17:44 +0200

    Fix colour coding so obvious when tests fail that should pass

--- spec/models/object_spec.rb
+++ spec/models/object_spec.rb
@@ -4,11 +4,11 @@ require 'spec_helper'
 
 describe Object do
   describe '#this_method_does_not_exist' do
-    it 'should return true (\e[33mexpect failure\e[0m)' do
+    it "should return true (\e[33mexpect failure\e[0m)" do
       subject.this_method_does_not_exist.should == true
     end
 
-    it 'should return true (\e[32mexpect success\e[0m)' do
+    it "should return true (\e[32mexpect success\e[0m)" do
       subject.kind_of?(Object).should == true
     end
   end

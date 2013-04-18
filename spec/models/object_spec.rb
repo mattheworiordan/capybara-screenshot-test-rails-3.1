c8cbe37ed65081b570507928acaee5e8c14379c4
Matthew O'Riordan <matthew.oriordan@gmail.com>
2013-04-18 12:47:06 +0200

    Test that model tests don't generate screenshots

--- spec/models/object_spec.rb
+++ spec/models/object_spec.rb
@@ -3,13 +3,15 @@
 require 'spec_helper'
 
 describe Object do
-  describe '#this_method_does_not_exist' do
-    it "should return true (\e[33mexpect failure\e[0m)" do
-      subject.this_method_does_not_exist.should == true
-    end
+  context 'Models should not capture screenshots' do
+    describe '#this_method_does_not_exist' do
+      it "should return true (\e[33mexpect failure\e[0m)" do
+        subject.this_method_does_not_exist.should == true
+      end
 
-    it "should return true (\e[32mexpect success\e[0m)" do
-      subject.kind_of?(Object).should == true
+      it "should return true (\e[32mexpect success\e[0m)" do
+        subject.kind_of?(Object).should == true
+      end
     end
   end
 end
